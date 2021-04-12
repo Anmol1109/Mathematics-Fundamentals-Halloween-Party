ae3de2d2d1e0217d2c58d164d8f0ff4eebf952ca
ANMOL PANT <48082902+Anmol1109@users.noreply.github.com>
2021-04-12 23:20:26 +0530

    Add files via upload

---
+++ Solution.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+from __future__ import print_function
+
+import os
+import sys
+
+#
+# Complete the halloweenParty function below.
+#
+def halloweenParty(k):
+    return (k / 2) * (k / 2) + (k / 2) * (k % 2)
+    #
+    # Write your code here.
+    #
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(raw_input())
+
+    for t_itr in xrange(t):
+        k = int(raw_input())
+
+        result = halloweenParty(k)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
